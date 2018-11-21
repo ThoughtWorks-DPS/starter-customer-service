6321ea670bb422aae0738d84350f19f84006d597
Ranbir Chawla <rchawla@thoughtworks.com>
2018-11-20 18:50:36 -0700

    changed infra setup to a single run command

--- k8/create-gcloud-kubectl-context.sh
+++ k8/create-gcloud-kubectl-context.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 # assumes $KEY is set in the env by the CI tool
-PROJECT_ID=twdps-k8-starter
-CLUSTER_NAME=k8startercluster
 
 openssl aes-256-cbc -d -in gcloud-secret -k $KEY > gcloud-service-account.json
 gcloud auth activate-service-account k8starter-cluster-dev-admin@twdps-k8-starter.iam.gserviceaccount.com --key-file=gcloud-service-account.json
 rm -f gcloud-service-account.json
-gcloud container clusters get-credentials $CLUSTER_NAME --zone us-central1-a --project $PROJECT_ID
+gcloud container clusters get-credentials GKE_CLUSTER_NAME --zone us-central1-a --project GCLOUD_PROJECT_ID
 
