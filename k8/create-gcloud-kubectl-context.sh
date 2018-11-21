35c7b63690b4a98327e17b267c65a16d63ffbab4
Ranbir Chawla <rchawla@thoughtworks.com>
2018-11-20 18:47:23 -0700

    fixed bad cut and paste in shell script

--- k8/create-gcloud-kubectl-context.sh
+++ k8/create-gcloud-kubectl-context.sh
@@ -3,8 +3,8 @@
 PROJECT_ID=twdps-k8-starter
 CLUSTER_NAME=k8startercluster
 
-run: openssl aes-256-cbc -d -in gcloud-secret -k $KEY > gcloud-service-account.json
-run: gcloud auth activate-service-account k8starter-cluster-dev-admin@twdps-k8-starter.iam.gserviceaccount.com --key-file=gcloud-service-account.json
+openssl aes-256-cbc -d -in gcloud-secret -k $KEY > gcloud-service-account.json
+gcloud auth activate-service-account k8starter-cluster-dev-admin@twdps-k8-starter.iam.gserviceaccount.com --key-file=gcloud-service-account.json
 rm -f gcloud-service-account.json
 gcloud container clusters get-credentials $CLUSTER_NAME --zone us-central1-a --project $PROJECT_ID
 
