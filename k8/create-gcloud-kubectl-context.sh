81710ebd2cb1b29bb5d25b404ff84972703d3eba
Ranbir Chawla <rchawla@thoughtworks.com>
2018-11-20 18:42:43 -0700

    major re-write using re-usable pieces

---
+++ k8/create-gcloud-kubectl-context.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# assumes $KEY is set in the env by the CI tool
+PROJECT_ID=twdps-k8-starter
+CLUSTER_NAME=k8startercluster
+
+run: openssl aes-256-cbc -d -in gcloud-secret -k $KEY > gcloud-service-account.json
+run: gcloud auth activate-service-account k8starter-cluster-dev-admin@twdps-k8-starter.iam.gserviceaccount.com --key-file=gcloud-service-account.json
+rm -f gcloud-service-account.json
+gcloud container clusters get-credentials $CLUSTER_NAME --zone us-central1-a --project $PROJECT_ID
+
--- k8/create-kubectl-context.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# assumes $KEY is set in the env by the CI tool
-PROJECT_ID=twdps-k8-starter
-CLUSTER_NAME=k8startercluster
-
-gcloud container clusters get-credentials $CLUSTER_NAME --zone us-central1-a --project $PROJECT_ID
-
-# remove key file, not needed anymore
